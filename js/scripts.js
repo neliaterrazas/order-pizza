98019dd949e4d50b2ac9b537cdb1993a3c54e3e8
Nelia Terrazas <terrazasnelia@gmail.com>
2019-02-01 11:28:51 -0800

    add variables for form values

--- js/scripts.js
+++ js/scripts.js
@@ -5,5 +5,23 @@ function Pizza (size, toppings, cost) {
   this.cost = cost;
 }
 
-var classicCheese = new Pizza ();
-var classicPepperoni = new Pizza ();
+// var classicCheese = new Pizza ();
+// var classicPepperoni = new Pizza ();
+
+
+$(document).ready(function() {
+
+  $("form#formOne").submit(function(event) {
+
+  event.preventDefault();
+
+  var name = $("#name").val();
+  var size = $("#size").val();
+  var veggies = $("#veggies").val();
+  var carnes = $("#carnes").val();
+
+  $("#results").show();
+
+}
+  });
+});
